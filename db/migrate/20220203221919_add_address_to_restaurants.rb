39ec76e8b5a0b8af5a6c7934012cce396cb2982f
barbara santos <barbara.santoss@hotmail.com>
2022-02-03 21:30:25 -0300

    add some card style

--- db/migrate/20220203221919_add_address_to_restaurants.rb
+++
@@ -1,7 +0,0 @@
-class AddAddressToRestaurants < ActiveRecord::Migration[6.1]
-  def change
-    add_column :restaurants, :address, :string
-    add_column :restaurants, :description_restaurant, :text
-    add_column :restaurants, :picture_url, :string
-  end
-end
