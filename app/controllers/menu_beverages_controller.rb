3dee5582776e64fa4f68445c296cc074537f91d8
barbara santos <barbara.santoss@hotmail.com>
2022-02-06 21:14:04 -0300

    created views

--- app/controllers/menu_beverages_controller.rb
+++ app/controllers/menu_beverages_controller.rb
@@ -1,2 +1,7 @@
 class MenuBeveragesController < ApplicationController
+  def new
+    # we need @restaurant in our `simple_form_for`
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @menu_meal = Menu_meal.new
+  end
 end
--- app/controllers/menu_meals_controller.rb
+++ app/controllers/menu_meals_controller.rb
@@ -1,2 +1,7 @@
 class MenuMealsController < ApplicationController
+  def new
+    # we need @restaurant in our `simple_form_for`
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @menu_beverage = Menu_beverage.new
+  end
 end
